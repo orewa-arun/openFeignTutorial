8dc8cfebdb85dd2f515525419825d7e625c7975e
orewa-arun <arunworkacc0@gmail.com>
2024-08-25 18:24:03 +0530

    employee response

---
+++ employeeService/src/main/java/com/openfeign/employeeService/dto/EmployeeDTO.java
@@ -0,0 +1,14 @@
+package com.openfeign.employeeService.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmployeeDTO {
+
+    private int id;
+    private String name;
+    private String email;
+    private int age;
+}
