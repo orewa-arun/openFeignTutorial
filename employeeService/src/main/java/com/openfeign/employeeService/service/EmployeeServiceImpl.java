8f1a0e2fd5ad4ee58625c42c508b2efb9cd70e9e
orewa-arun <arunworkacc0@gmail.com>
2024-08-25 20:41:59 +0530

    AddressClient used to call getAddressByEmployeeId from address-service

--- employeeService/src/main/java/com/openfeign/employeeService/service/EmployeeServiceImpl.java
+++ employeeService/src/main/java/com/openfeign/employeeService/service/EmployeeServiceImpl.java
@@ -1,9 +1,12 @@
 package com.openfeign.employeeService.service;
+import com.openfeign.employeeService.dto.AddressDTO;
 import com.openfeign.employeeService.dto.EmployeeDTO;
 import com.openfeign.employeeService.entity.Employee;
+import com.openfeign.employeeService.feignClient.AddressClient;
 import com.openfeign.employeeService.repository.EmployeeRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -16,12 +19,18 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Autowired
     private ModelMapper modelMapper;
 
+    @Autowired
+    private AddressClient addressClient;
+
     @Override
     public EmployeeDTO getEmployeeById(int id) throws Exception {
         Optional<Employee> optional = employeeRepository.findById(id);
         if(optional.isPresent()) {
             Employee employee = optional.get();
-            return modelMapper.map(employee, EmployeeDTO.class);
+            EmployeeDTO employeeDTO = modelMapper.map(employee, EmployeeDTO.class);
+            ResponseEntity<AddressDTO> address = addressClient.getAddressByEmployeeId(id);
+            employeeDTO.setAddressDTO(address.getBody());
+            return employeeDTO;
         }else {
             throw new Exception("Employee with id " + id + " not found!");
         }
